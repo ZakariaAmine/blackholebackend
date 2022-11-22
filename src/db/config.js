2822841cbd23debbb1e5da043f4ab77d59ecbb4d
thomasr2240 <thomasr2240@gmail.com>
2022-11-22 18:06:34 +0900

    Update config.js

--- src/db/config.js
+++ src/db/config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    url: "mongodb://localhost:27017/blackhole"
+    // url: "mongodb://localhost:27017/blackhole"
     // url: "mongodb+srv://root:rootroot@cluster0.okeov.mongodb.net/pink?retryWrites=true&w=majority"
+    url:"mongodb+srv://vercel-admin-user:BLBfhTcCtuIc3NKx@cluster0.52niv7k.mongodb.net/blackhole?retryWrites=true&w=majority"
 }
